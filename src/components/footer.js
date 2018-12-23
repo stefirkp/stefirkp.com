72f29a488fa8edce50261dab88a26c532ca38ca1
stefirkp <stefirkp@gmail.com>
2018-12-23 20:46:22 +0700

    Update gs-config"
    ;

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => (
+  <footer>
+    What next ? 
+    <div className="photo"></div>
+    stefirkp@gmail.com 
+    <div className="socmed-wrap">
+      <span>Linkin</span>
+      <span>FB</span>
+      <span>IG</span>
+    </div>
+  </footer>
+)
+
+export default Footer
